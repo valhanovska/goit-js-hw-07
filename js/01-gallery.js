f9e1b3d9f90a44bad4323d34fe5a9301c067993d
Valentyna Hanovska <riefenstahl.valli@gmail.com>
2022-08-09 23:05:27 +0300

    add 01 task

---
+++ js/01-gallery.js
@@ -0,0 +1,49 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+
+function createGallery() {
+  let imgCard = galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href=${original}>
+    <img
+      class="gallery__image"
+      src=${preview}
+      data-source=${original}
+      alt=${description}
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+  galleryEl.innerHTML = imgCard;
+}
+createGallery();
+
+galleryEl.addEventListener("click", onImgClick);
+function onImgClick(event) {
+  event.preventDefault();
+
+  const srcOriginalImages = event.target.dataset.source;
+
+  const instance = basicLightbox.create(`
+    <img src=${srcOriginalImages} width="800" height="600">
+    `);
+
+  instance.show();
+
+  document.addEventListener("keydown", closeModalEsc);
+
+  function closeModalEsc(e) {
+    if (e.code !== "Escape") return;
+    else {
+      instance.close();
+
+      document.removeEventListener("keydown", closeModalEsc);
+    }
+  }
+}
