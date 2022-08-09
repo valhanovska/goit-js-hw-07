be99af129c91bbdf5f9786f6552abe23d0de0528
Valentyna Hanovska <riefenstahl.valli@gmail.com>
2022-08-09 23:05:37 +0300

    add 2 task

---
+++ js/02-lightbox.js
@@ -0,0 +1,28 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+function createGallery() {
+  let imgCard = galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href=${original}>
+    <img
+      class="gallery__image"
+      src=${preview}
+      alt=${description}
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+  galleryEl.innerHTML = imgCard;
+}
+createGallery();
+
+var lightbox = new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+  captionsData: "alt",
+  captionPosition: "bottom",
+});
