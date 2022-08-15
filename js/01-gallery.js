004c75c93d23a5d5c5eb774e4afea446cd313597
Valentyna Hanovska <riefenstahl.valli@gmail.com>
2022-08-15 22:35:21 +0300

    fixed

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,8 +1,6 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
-
 const galleryEl = document.querySelector(".gallery");
 
 function createGallery() {
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,6 +1,5 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-console.log(galleryItems);
 
 const galleryEl = document.querySelector(".gallery");
 function createGallery() {
